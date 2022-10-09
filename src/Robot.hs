69a8c007a4a909647dcdfe7be4ca17d57c565bdf
Cecote <thiago.cecote@aluno.ufop.edu.br>
2022-10-09 14:12:39 -0300

    Utilizando replace no exercicio 5

--- src/Robot.hs
+++ src/Robot.hs
@@ -106,21 +106,21 @@ exampleMine :: Mine
 exampleMine = Mine {
                 lines = 15,
                 columns = 15,
-                elements = [[%,%,%,%,%,%,%,%,%,%,%,%,%,%,%], 
-                            [%,*,*,*,.,.,.,.,.,.,.,.,.,.,%],
-                            [%,*,*,*,.,.,., ,.,.,.,*,.,.,%],
-                            [%,*,*,*,.,.,., ,.,.,*,*,*,.,%],
-                            [%,.,?,.,.,.,., ,.,.,.,*,.,.,%],
-                            [%,.,., , , , , ,.,., ,.,.,.,%],
-                            [%,.,.,.,., ,.,.,.,., ,.,.,.,%],
-                            [%,.,:,.,., ,.,.,.,., ,.,.,.,%],
-                            [%,.,., ,., , , , , , , ,.,.,%],
-                            [%,.,.,*,., ,.,., ,.,.,.,.,.,%],
-                            [%,.,.,.,., ,.,., ,.,;,;,.,.,%],
-                            [%,.,*,.,., ,.,.,.,;,;,.,.,*,%],
-                            [%,.,.,.,.,.,.,.,.,.,.,.,.,$,%],
-                            [%,.,.,.,.,.,.,.,.,., , , ,.,%],
-                            [%,%,%,%,%,%,%,%,%,%,%,%,%,L,%],]
+                elements = [[Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall], 
+                            [Wall,Rock,Rock,Rock,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Wall],
+                            [Wall,Rock,Rock,Rock,Earth,Earth,Earth,Empty,Earth,Earth,Earth,Rock,Earth,Earth,Wall],
+                            [Wall,Rock,Rock,Rock,Earth,Earth,Earth,Empty,Earth,Earth,Rock,Rock,Rock,Earth,Wall],
+                            [Wall,Earth,(Material 50),Earth,Earth,Earth,Earth,Empty,Earth,Earth,Earth,Rock,Earth,Earth,Wall],
+                            [Wall,Earth,Earth,Empty,Empty,Empty,Empty,Empty,Earth,Earth,Empty,Earth,Earth,Earth,Wall],
+                            [Wall,Earth,Earth,Earth,Earth,Empty,Earth,Earth,Earth,Earth,Empty,Earth,Earth,Earth,Wall],
+                            [Wall,Earth,(Material 100),Earth,Earth,Empty,Earth,Earth,Earth,Earth,Empty,Earth,Earth,Earth,Wall],
+                            [Wall,Earth,Earth,Empty,Earth,Empty,Empty,Empty,Empty,Empty,Empty,Empty,Earth,Earth,Wall],
+                            [Wall,Earth,Earth,Rock,Earth,Empty,Earth,Earth,Empty,Earth,Earth,Earth,Earth,Earth,Wall],
+                            [Wall,Earth,Earth,Earth,Earth,Empty,Earth,Earth,Empty,Earth,(Material 150),(Material 150),Earth,Earth,Wall],
+                            [Wall,Earth,Rock,Earth,Earth,Empty,Earth,Earth,Earth,(Material 150),(Material 150),Earth,Earth,Rock,Wall],
+                            [Wall,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,(Material 1),Wall],
+                            [Wall,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Earth,Empty,Empty,Empty,Earth,Wall],
+                            [Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Entry,Wall]]
             }
 
 pLine :: Parser Char Line
